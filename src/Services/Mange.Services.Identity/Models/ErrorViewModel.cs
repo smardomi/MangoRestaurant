9922ca34cfb65fbd3ba450439fd272663c964497
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-11-20 15:39:31 +0330

    Integrate QuickStart in Identity Server Service

---
+++ src/Services/Mange.Services.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mange.Services.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
